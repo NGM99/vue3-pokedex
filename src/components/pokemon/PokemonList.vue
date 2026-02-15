06282daee529732b3eb5cdbeab77ba4981d4f3f6
NG <noel_alejandro10@hotmail.com>
2026-02-15 12:13:17 -0600

    implement getPokemon service

---
+++ src/components/pokemon/PokemonList.vue
@@ -0,0 +1,108 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import { getPokemons } from '../../services/pokemonService'
+
+const pokemons = ref([])
+const isLoading = ref(false)
+const error = ref(null)
+
+const fetchPokemons = async () => {
+	isLoading.value = true
+	error.value = null
+
+	try {
+		const data = await getPokemons()
+		pokemons.value = data.results
+
+	} catch {
+		error.value = 'Error al cargar Pokémon'
+	} finally {
+		isLoading.value = false
+	}
+}
+
+onMounted(() => {
+	fetchPokemons()
+})
+</script>
+
+<template>
+	<div class="container">
+		<div class="title-group">
+			<img src="../../assets/images/pokebola_logo_light-mode.png" alt="Pokeball" class="title-icon" />
+			<h1 class="title">Pokédex</h1>
+		</div>
+
+		<div class="grid">
+			<div
+				v-for="pokemon in pokemons"
+				:key="pokemon.id"
+				class="card"
+			>
+				<img :src="pokemon.image" :alt="pokemon.name" class="pokemon-img" />
+				<h2 class="pokemon-name">{{ pokemon.name }}</h2>
+			</div>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+.container {
+	padding: 1rem;
+	max-width: 1200px;
+	margin: auto;
+}
+
+.title {
+	font-size: 2rem;
+	color: #ffcb05;
+}
+
+.title-group {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	gap: 0.8rem;
+}
+
+.title-icon {
+	width: 32px;
+	height: 32px;
+}
+
+.grid {
+	display: grid;
+	gap: 1.5rem;
+	grid-template-columns: repeat(auto-fit, minmax(220px, 280px));
+	justify-content: center;
+}
+
+.card {
+	width: 80%;
+	background: white;
+	border-radius: 16px;
+	padding: 1.5rem;
+	text-align: center;
+	box-shadow: 0 6px 18px rgba(0, 0, 0, 0.08);
+	transition: transform 0.2s ease, box-shadow 0.2s ease;
+	cursor: pointer;
+}
+
+.card:hover {
+	transform: translateY(-5px);
+	box-shadow: 0 10px 22px rgba(0, 0, 0, 0.15);
+}
+
+.pokemon-img {
+	width: 120px;
+	height: 120px;
+	object-fit: contain;
+	margin-bottom: 1rem;
+}
+
+.pokemon-name {
+	font-size: 1.2rem;
+	font-weight: 600;
+	text-transform: capitalize;
+}
+</style>
---
+++ src/services/pokemonService.js
@@ -0,0 +1,33 @@
+import axios from "axios"
+
+const api = axios.create({
+	baseURL: "https://pokeapi.co/api/v2",
+	timeout: 5000
+})
+
+const getPokemons = async (offset = 0, limit = 20) => {
+	const response = await api.get(`/pokemon?offset=${offset}&limit=${limit}`)
+
+	const { count, next, previous, results } = response.data
+
+	const formattedResults = results.map(pokemon => {
+		const id = pokemon.url.split('/').at(-2)
+
+		return {
+			id,
+			name: pokemon.name,
+			image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`
+		}
+	})
+
+	return {
+		count,
+		next,
+		previous,
+		results: formattedResults
+	}
+}
+
+export {
+	getPokemons
+}
