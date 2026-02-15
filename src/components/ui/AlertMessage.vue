34a2f3b54c38115ae647c831dec6549605f2b54a
NG <noel_alejandro10@hotmail.com>
2026-02-15 14:42:20 -0600

    add AlertMessage component

---
+++ src/components/ui/AlertMessage.vue
@@ -0,0 +1,35 @@
+<script setup>
+defineProps({
+	type: {
+		type: String,
+		default: "success",
+	}
+})
+</script>
+
+<template>
+	<div class="alert" :class="type" role="alert">
+		<slot />
+	</div>
+</template>
+
+<style scoped>
+.alert {
+	padding: 1rem 1.5rem;
+	border-radius: 10px;
+	font-weight: 500;
+	margin-bottom: 1rem;
+	transition: 0.2s ease;
+	margin-top: 1rem;
+}
+
+.alert.success {
+	background-color: #e6f4ea;
+	color: #1e7e34;
+}
+
+.alert.danger {
+	background-color: #f8d7da;
+	color: #842029;
+}
+</style>
