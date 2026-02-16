443a5552c07be9deedf9b60ac9b7a5cc24ca1e65
NG <noel_alejandro10@hotmail.com>
2026-02-16 11:02:11 -0600

    add Header component and implement

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script setup>
 import PokemonList from './components/pokemon/PokemonList.vue'
+import Header from '../src/components/layout/Header.vue'
 </script>
 
 <template>
-  <PokemonList />
+  <div>
+    <Header title="Pokédex" />
+    <PokemonList />
+  </div>
 </template>
 
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,31 @@
+<script setup>
+import pokemonLogo from '../../assets/images/International_Pokémon_logo.svg'
+</script>
+
+<template>
+  <header class="header">
+    <div class="title-group">
+      <img
+        :src="pokemonLogo"
+        alt="Pokémon"
+        class="title-logo"
+      />
+    </div>
+  </header>
+</template>
+
+<style scoped>
+.header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #E53935;
+  border-bottom: solid 3px #2C2C2C;
+  height: 70px;
+}
+
+.title-logo {
+  height: 55px;
+  object-fit: contain;
+}
+</style>
